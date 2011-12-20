4183ceb07e31da4aca9224ef6feac2e377344262
ELIN <elin@mikomoe.jp>
2011-12-20 16:55:32 +0900

    -

--- Ignorance.pm
+++ Ignorance.pm
@@ -13,33 +13,29 @@ sub new:method
 	my $a = shift();
 	my %a = @_;
 
-	return(bless({callback =>$a,map{$_ =>$_->new(@{$a{$_}})}qw(CGI CGI::Session Text::Xslate)},$j));
+	return(bless({callback =>$a,args =>\%a,map{$_,$_->new(@{$a{$_}})}qw(CGI Text::Xslate)},$j));
 }
 
 sub perform:method
 {
 	my $j = shift();
-	my $a = shift();
 	my %a = @_;
 
-	my %_COOKIE = CGI::Cookie->fetch();
-	$j->{CGI::Session}->load(defined($_COOKIE{IGNORANCE_SESSION}) ? $_COOKIE{IGNORANCE_SESSION}->value() : undef);
-
-	local %ENV = (%ENV,SESSION =>$j->{CGI::Session}->id());
-	local %SES = %{$j->{CGI::Session}->dataref()};
-	local %COOKIE = map{$_ =>$_->value()}keys(%_COOKIE);
+	local %ENV = %ENV;
 	local %GET;
 	local %POST;
-	my($engine,%r) = &{$j->{callback}->{(grep{$ENV{PATH_INFO} =~ $_}keys(%{$j->{callback}}))[0]}}();
-	if($engine eq "none"){
-	}elsif($engine eq "jump"){
-	}elsif($engine eq "Text::Xslate"){
+	local %COOKIE = map{$_->name(),$_->value()}grep{ref($_)}CGI::Cookie->fetch();
+	local %SES = %{($j->{CGI::Session} = CGI::Session->new($j->{args}->{CGI::Session}->[0],$COOKIE{IGNORANCE_SESSION},$j->{args}->{CGI::Session}->[2]))->dataref()};
+	my($order,%r) = &{$j->{callback}->{(grep{$ENV{PATH_INFO} =~ $_}keys(%{$j->{callback}}))[0]}}();
+	if($order eq "none"){
+	}elsif($order eq "jump"){
+	}elsif($order eq "Text::Xslate"){
 		$a->{ENV} = \%ENV;
 		$a->{SES} = \%SES;
 		$a->{COOKIE} = \%COOKIE;
 		$a->{GET} = \%GET;
 		$a->{POST} = \%POST;
-		print $j->{CGI}->header(qw(-type text/html -charset UTF-8 -cookie),[$j->{}->cookie("IGNORANCE_SESSION",$ENV{SESSION})]);
+		print $j->{CGI}->header(qw(-type text/html -charset UTF-8 -cookie),[$j->{CGI}->cookie(qw(-name IGNORANCE_SESSION -value),$j->{CGI::Session}->id())]);
 		print $j->{Text::Xslate}->render($r{file},$a);
 	}
 	return();
