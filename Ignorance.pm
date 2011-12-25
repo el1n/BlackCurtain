507cb1961786d9d8774a3900ed6f491a2bdfc2af
ELIN <elin@mikomoe.jp>
2011-12-25 12:29:53 +0900

    -

--- Ignorance.pm
+++ Ignorance.pm
@@ -6,6 +6,7 @@ use CGI;
 use CGI::Session;
 use CGI::Cookie;
 use Text::Xslate;
+use XML::Simple;
 
 sub new:method
 {
@@ -13,7 +14,7 @@ sub new:method
 	my $a = shift();
 	my %a = @_;
 
-	return(bless({callback =>$a,args =>\%a,map{$_,$_->new(@{$a{$_}})}qw(CGI Text::Xslate)},$j));
+	return(bless({callback =>$a,args =>\%a,map{$_,$_->new(@{$a{$_}})}qw(CGI Text::Xslate XML::Simple)},$j));
 }
 
 sub perform:method
@@ -27,9 +28,9 @@ sub perform:method
 	local %COOKIE = map{$_->name(),$_->value()}grep{ref($_)}CGI::Cookie->fetch();
 	local %SES = %{($j->{CGI::Session} = CGI::Session->new($j->{args}->{CGI::Session}->[0],$COOKIE{IGNORANCE_SESSION},$j->{args}->{CGI::Session}->[2]))->dataref()};
 	my($issue,$d,%r) = &{$j->{callback}->{(grep{$ENV{PATH_INFO} =~ $_}keys(%{$j->{callback}}))[0]}}();
-	if($issue eq "none"){
-	}elsif($issue eq "jump"){
-	}elsif($issue eq "Text::Xslate"){
+	if($issue =~ /^none$/io){
+	}elsif($issue =~ /^jump$/io){
+	}elsif($issue =~ /^(?:Text::)?Xslate$/io){
 		$d->{ENV} = \%ENV;
 		$d->{SES} = \%SES;
 		$d->{COOKIE} = \%COOKIE;
@@ -38,6 +39,9 @@ sub perform:method
 		$d->{URL} = sub($){return($ENV{SCRIPT_NAME}.shift())};
 		print $j->{CGI}->header(qw(-type text/html -charset UTF-8 -cookie),[$j->{CGI}->cookie(qw(-name IGNORANCE_SESSION -value),$j->{CGI::Session}->id())]);
 		print $j->{Text::Xslate}->render($r{file},$d);
+	}elsif($issue =~ /^XML(?:::Simple)?$/io){
+		print $j->{CGI}->header(qw(-type application/xml -charset UTF-8 -cookie),[$j->{CGI}->cookie(qw(-name IGNORANCE_SESSION -value),$j->{CGI::Session}->id())]);
+		print $j->{XML::Simple}->XMLout($d);
 	}
 	return();
 }
