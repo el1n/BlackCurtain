8bd9196dd58ae8c149c36e5bfdbc7ba9a094f536
ELIN <elin@mikomoe.jp>
2012-01-16 23:10:35 +0900

    -

--- Ignorance.pm
+++ Ignorance.pm
@@ -33,56 +33,65 @@ sub perform:method
 	my %POST = $ENV{REQUEST_METHOD} eq "POST" ? map{$_,join(" ",$s->{CGI}->param($_))}$s->{CGI}->param() : undef;
 	my %QUERY = (%GET,%POST);
 
-	my $sub = $s->{callback}->{(grep{$ENV{PATH_INFO} =~ $_}keys(%{$s->{callback}}))[0]};
-	my $pkg = Mouse::Util::get_code_package($sub);
-	local *{$pkg."::ENV"} = \%ENV;
-	local *{$pkg."::COOKIE"} = \%COOKIE;
-	local *{$pkg."::SES"} = \%SES;
-	local *{$pkg."::GET"} = \%GET;
-	local *{$pkg."::POST"} = \%POST;
-	local *{$pkg."::QUERY"} = \%QUERY;
+	my $pass = 0;
+	for my $callback (@{$s->{callback}}){
+		my($regex,$sub,@args) = @{$callback};
 
-	my($issue,$d,%r) = $sub->([$ENV{PATH_INFO} =~m/\/+([0-9A-Za-z_]+)/o]);
-	push(@{$r{cookie}},$s->{CGI}->cookie(qw(-name IGNORANCE_SID -value),$s->{CGI::Session}->id()));
-	if($issue =~ /^none$/io){
-		print $s->{CGI}->header(qw(-type text/html -charset UTF-8 -cookie),$r{cookie});
-	}elsif($issue =~ /^data$/io){
-		print $s->{CGI}->header(qw(-type text/html -charset UTF-8 -cookie),$r{cookie});
-		print $d;
-	}elsif($issue =~ /^file$/io){
-	}elsif($issue =~ /^jump$/io){
-		print $s->{CGI}->redirect(qw(-url),$d,qw(-cookie),$r{cookie});
-	}elsif($issue =~ /^(?:Text::)?Xslate$/io){
-		$s->{Text::Xslate} ||= Text::Xslate->new(@{$s->{args}->{Text::Xslate}});
-		$d->{ENV} = \%ENV;
-		$d->{SES} = \%SES;
-		$d->{COOKIE} = \%COOKIE;
-		$d->{GET} = \%GET;
-		$d->{POST} = \%POST;
-		$d->{URL} = sub($){return($ENV{SCRIPT_NAME}.shift())};
-		print $s->{CGI}->header(qw(-type text/html -charset UTF-8 -cookie),$r{cookie});
-		print $s->{Text::Xslate}->render($r{file},$d);
-	}elsif($issue =~ /^XML(?:::Simple)?$/io){
-		print $s->{CGI}->header(qw(-type application/xml -charset UTF-8 -cookie),$r{cookie});
-		print XML::Simple::XMLout($d);
-	}elsif($issue =~ /^YAML(?:::Syck)?$/io){
-		print $s->{CGI}->header(qw(-type text/plain -charset UTF-8 -cookie),$r{cookie});
-		print YAML::Syck::Dump($d);
-	}elsif($issue =~ /^JSON(?:::Syck)?$/io){
-		print $s->{CGI}->header(qw(-type application/json -charset UTF-8 -cookie),$r{cookie});
-		print JSON::Syck::Dump($d);
-	}elsif($issue =~ /^(?:Data::)?Dumper$/io){
-		$d->{ENV} = \%ENV;
-		$d->{SES} = \%SES;
-		$d->{COOKIE} = \%COOKIE;
-		$d->{GET} = \%GET;
-		$d->{POST} = \%POST;
-		print $s->{CGI}->header(qw(-type text/plain -charset UTF-8 -cookie),$r{cookie});
-		print Data::Dumper::Dumper($d);
+		if($ENV{PATH_INFO} =~ $regex){
+			my @m = ($1,$2,$3,$4,$5,$6,$7,$8,$9);
+
+			my $pkg = Mouse::Util::get_code_package($sub);
+			local *{$pkg."::ENV"} = \%ENV;
+			local *{$pkg."::COOKIE"} = \%COOKIE;
+			local *{$pkg."::SES"} = \%SES;
+			local *{$pkg."::GET"} = \%GET;
+			local *{$pkg."::POST"} = \%POST;
+			local *{$pkg."::QUERY"} = \%QUERY;
+		
+			my($issue,$d,%r) = $sub->([$ENV{PATH_INFO} =~m/\/+([0-9A-Za-z_]+)/o],\@m,\@args,{pass =>$pass++});
+			push(@{$r{cookie}},$s->{CGI}->cookie(qw(-name IGNORANCE_SID -value),$s->{CGI::Session}->id()));
+			if($issue =~ /^none$/io){
+				print $s->{CGI}->header(qw(-type text/html -charset UTF-8 -cookie),$r{cookie});
+			}elsif($issue =~ /^data$/io){
+				print $s->{CGI}->header(qw(-type text/html -charset UTF-8 -cookie),$r{cookie});
+				print $d;
+			}elsif($issue =~ /^file$/io){
+			}elsif($issue =~ /^jump$/io){
+				print $s->{CGI}->redirect(qw(-url),$d,qw(-cookie),$r{cookie});
+			}elsif($issue =~ /^(?:Text::)?Xslate$/io){
+				$s->{Text::Xslate} ||= Text::Xslate->new(@{$s->{args}->{Text::Xslate}});
+				$d->{ENV} = \%ENV;
+				$d->{SES} = \%SES;
+				$d->{COOKIE} = \%COOKIE;
+				$d->{GET} = \%GET;
+				$d->{POST} = \%POST;
+				$d->{URL} = sub($){return($ENV{SCRIPT_NAME}.shift())};
+				print $s->{CGI}->header(qw(-type text/html -charset UTF-8 -cookie),$r{cookie});
+				print defined($r{file}) ? $s->{Text::Xslate}->render($r{file},$d) : $s->{Text::Xslate}->render_string($r{data},$d);
+			}elsif($issue =~ /^XML(?:::Simple)?$/io){
+				print $s->{CGI}->header(qw(-type application/xml -charset UTF-8 -cookie),$r{cookie});
+				print XML::Simple::XMLout($d);
+			}elsif($issue =~ /^YAML(?:::Syck)?$/io){
+				print $s->{CGI}->header(qw(-type text/plain -charset UTF-8 -cookie),$r{cookie});
+				print YAML::Syck::Dump($d);
+			}elsif($issue =~ /^JSON(?:::Syck)?$/io){
+				print $s->{CGI}->header(qw(-type application/json -charset UTF-8 -cookie),$r{cookie});
+				print JSON::Syck::Dump($d);
+			}elsif($issue =~ /^(?:Data::)?Dumper$/io){
+				$d->{ENV} = \%ENV;
+				$d->{SES} = \%SES;
+				$d->{COOKIE} = \%COOKIE;
+				$d->{GET} = \%GET;
+				$d->{POST} = \%POST;
+				print $s->{CGI}->header(qw(-type text/plain -charset UTF-8 -cookie),$r{cookie});
+				print Data::Dumper::Dumper($d);
+			}
+		}
 	}
 	$s->{CGI::Session}->{_DATA} = \%SES;
 	$s->{CGI::Session}->save_param();
 	$s->{CGI::Session}->close();
+
 	return();
 }
 
